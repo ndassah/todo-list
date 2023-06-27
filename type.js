c622b58bd4e740138834d84e83c555d7dbdfa220
ndassah <mohamedtariq150@gmail.com>
2023-06-27 17:43:36 +0200

    mise a jour du script

--- type.js
+++ type.js
@@ -3,5 +3,14 @@ var toDocontainer = document.getElementById('toDoContainer');
 var inputField = document.getElementById('inputField');
 
 addToDoButton.onclick = function(){
-    alert('vous avez clicque')
+   //verifie si l input n'est pas vide
+   if(inputField.value !=""){
+    //si l'input n'est pas  vide , créer un paragraphe
+    var paragraphe = document.createElement('p')
+   }
+   //insere uncontenue dans le paragraphe
+   paragraphe.innerText = inputField.value;
+
+   //inserer le paragraphe dans l'element todolist
+   toDocontainer.appendChild(paragraphe);
 }
