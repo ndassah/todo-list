01109f7f7951ae7ae280ecd93abc9069eee78549
ndassah <mohamedtariq150@gmail.com>
2023-06-27 17:39:03 +0200

    ajout du script

---
+++ type.js
@@ -0,0 +1,7 @@
+var addToDoButton = document.getElementById('addTodo');
+var toDocontainer = document.getElementById('toDoContainer');
+var inputField = document.getElementById('inputField');
+
+addToDoButton.onclick = function(){
+    alert('vous avez clicque')
+}
